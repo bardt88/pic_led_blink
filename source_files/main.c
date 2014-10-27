3510196f440fec1068d3283e4db9d146fad3ff67
bardt88 <bartlaan@gmail.com>
2014-10-27 22:33:12 +0100

    added code

--- source_files/main.c
+++ source_files/main.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <htc.h>
 
-#pragma config MCLRE = ON, CP = OFF, WDTE = OFF
+#pragma config FOSC = INTRCIO   // Oscillator Selection bits (INTOSC oscillator: I/O function on GP4/OSC2/CLKOUT pin, I/O function on GP5/OSC1/CLKIN)
+#pragma config WDTE = ON        // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = OFF      // Power-Up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON       // GP3/MCLR pin function select (GP3/MCLR pin function is MCLR)
+#pragma config BOREN = ON       // Brown-out Detect Enable bit (BOD enabled)
+#pragma config CP = OFF         // Code Protection bit (Program Memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#define _XTAL_FREQ 4000000
 
 
-int main(int argc, char** argv) {
-
-    return (EXIT_SUCCESS);
+int main(void) {
+    GPIO = 0x00;
+    while(1) {
+     GP3 = 1;
+     __delay_ms(500);
+   GP3 = 0;
+   __delay_ms(500);
+    }
 }
 
